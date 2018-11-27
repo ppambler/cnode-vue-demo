9ba862b0a90c385b94585b43630978ecbcddffeb
Ambler <allinlbj@gmail.com>
2018-11-27 12:52:03 +0800

    add header nav

---
+++ vue-webpack-app/cnode-app/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <Header />
+  </div>
+</template>
+
+<script>
+import Header from './components/Header'
+export default {
+  name: 'App',
+  components: {
+    Header,
+  }
+}
+</script>
+
+<style>
+/* 样式初始化 */
+body,ol,ul,h1,h2,h3,h4,h5,h6,p,th,td,dl,dd,form,fieldset,legend,input,textarea,select{margin:0;padding:0} 
+a{color:#2d374b;text-decoration:none} 
+a:hover{color:#cd0200;text-decoration:underline} 
+em{font-style:normal} 
+li{list-style:none} 
+img{border:0;vertical-align:middle} 
+table{border-collapse:collapse;border-spacing:0} 
+p{word-wrap:break-word}
+*, *:before, *:after {
+  -webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  box-sizing: border-box;
+}
+
+
+</style>
---
+++ vue-webpack-app/cnode-app/src/components/Header.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="header">
+        <a href="#">
+            <img src="../assets/cnodejs_light.svg" alt="" width="120" height="28">
+        </a>
+        <ul>
+            <li><a href="#">首页</a></li>
+            <li><a href="#">新手入门</a></li>
+            <li><a href="#">API</a></li>
+            <li><a href="#">关于</a></li>
+            <li><a href="#">注册</a></li>
+            <li><a href="#">登录</a></li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  data () {
+    return {
+      msg: ''
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    .header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        height: 50px;
+        background: #444;
+    }
+    .header > a {
+        display: inline-block;
+        margin-left: 67px;
+        margin-bottom: 6px;
+    }
+    ul {
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        height: 100%;
+        width: 356px;
+        margin-right: 84px;
+    }
+    ul > li {
+        margin-bottom: 4px;
+    }
+    ul > li a {
+        display: inline-block;
+        color: #ccc;
+        font-size: 13px;
+    }
+    ul > li a:hover {
+        text-decoration: none;
+        color: #fff;
+    }
+</style>
