c223ba2999b306492fdb74bd1671ad21a57172a6
Ambler <allinlbj@gmail.com>
2019-04-17 10:49:15 +0800

    点击最后一个元素加1或者点击第一个元素（非第一页）就减一

--- vue-webpack-app/cnode-app/src/components/Pagination.vue
+++ vue-webpack-app/cnode-app/src/components/Pagination.vue
@@ -23,7 +23,17 @@ export default {
   },
   methods: {
     changeBtn(page) {
+      if(page == '……') return
       this.currentPage = page
+      if(page == this.pagebtns[4]) {
+        // 顺着来
+        this.pagebtns.shift() //把第一个元素给GG了
+        this.pagebtns.splice(4,0,this.pagebtns[3]+1) //退一步，进一步
+      } else if(page == this.pagebtns[0] && page != 1) {
+        // 倒着来
+        this.pagebtns.unshift(this.pagebtns[0]-1) //如果你点的是第一个元素，那么久移除最后一个元素，并把第一个元素替换成减一的结果
+        this.pagebtns.splice(5,1) //最后一个元素可以GG了
+      }
     }
   }
 };
