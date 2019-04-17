d7f5cb5d05c2e4c08f106104630f458f8257f983
Ambler <allinlbj@gmail.com>
2019-04-17 19:10:28 +0800

    添加jQuery，完成点击「首页、上一页、下一页」切换页面的功能

--- vue-webpack-app/cnode-app/src/components/Pagination.vue
+++ vue-webpack-app/cnode-app/src/components/Pagination.vue
@@ -1,18 +1,19 @@
 <template>
   <div class=".pagination">
-    <button>首页</button>
-    <button>上一页</button>
+    <button @click="changeBtn">首页</button>
+    <button @click="changeBtn">上一页</button>
     <button
       v-for="btn in pagebtns"
       :key="btn"
       :class="[{'currentPage': btn == currentPage},'pagebtn']"
       @click="changeBtn(btn)"
     >{{btn}}</button>
-    <button>下一页</button>
+    <button @click="changeBtn">下一页</button>
   </div>
 </template>
 
 <script>
+import $ from 'jquery'
 export default {
   name: "Pagination",
   data() {
@@ -23,16 +24,37 @@ export default {
   },
   methods: {
     changeBtn(page) {
-      if(page == '……') return
-      this.currentPage = page
-      if(page == this.pagebtns[4]) {
+      if (page == "……") return;
+      if (typeof page != "number") {
+        switch (page.target.innerText) {
+          case "上一页":
+            $("button.currentPage")
+              .prev()
+              .click();
+            break;
+          case "下一页":
+            $("button.currentPage")
+              .next()
+              .click();
+            break;
+          case "首页":
+            this.pagebtns = [1, 2, 3, 4, 5, "......"];
+            this.changeBtn(1);
+            break;
+          default:
+            break;
+        }
+        return;
+      }
+      this.currentPage = page;
+      if (page == this.pagebtns[4]) {
         // 顺着来
-        this.pagebtns.shift() //把第一个元素给GG了
-        this.pagebtns.splice(4,0,this.pagebtns[3]+1) //退一步，进一步
-      } else if(page == this.pagebtns[0] && page != 1) {
+        this.pagebtns.shift(); //把第一个元素给GG了
+        this.pagebtns.splice(4, 0, this.pagebtns[3] + 1); //退一步，进一步
+      } else if (page == this.pagebtns[0] && page != 1) {
         // 倒着来
-        this.pagebtns.unshift(this.pagebtns[0]-1) //如果你点的是第一个元素，那么久移除最后一个元素，并把第一个元素替换成减一的结果
-        this.pagebtns.splice(5,1) //最后一个元素可以GG了
+        this.pagebtns.unshift(this.pagebtns[0] - 1); //如果你点的是第一个元素，那么久移除最后一个元素，并把第一个元素替换成减一的结果
+        this.pagebtns.splice(5, 1); //最后一个元素可以GG了
       }
     }
   }
